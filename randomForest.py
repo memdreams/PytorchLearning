bd6c251e231352d0066d9073359d28b596755a74
Dreams <memdreams@me.com>
2018-02-19 20:49:21 -0500

    bagging example

---
+++ randomForest.py
@@ -0,0 +1,4 @@
+from sklearn.ensemble import BaggingClassifier
+from sklearn.neighbors import KNeighborsClassifier
+
+bagging = BaggingClassifier(KNeighborsClassifier(), max_samples=0.5, max_features=0.5)
